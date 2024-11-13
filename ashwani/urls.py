44a7837b7f2a0e0e88ae49e26cfe47816ae41a4e
Ashwani kumar <65064582+ashwani-199@users.noreply.github.com>
2024-11-13 17:44:03 +0530

    third

--- ashwani/urls.py
+++ ashwani/urls.py
@@ -1,28 +1,35 @@
-"""
-URL configuration for ashwani project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from ashwani.common_modules.mainServices import MainService
+
+
+
+
+admin.site.site_title = "Ashwani_Portfolio Admin"
+admin.site.site_header = "Ashwani_Portfolio Administration"
+admin.site.index_title = "Ashwani_Portfolio Administration"
 
 urlpatterns = [
     path('dashboard/', admin.site.urls),
     path('', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
-urlpatterns += staticfiles_urlpatterns()
+
+handler404 = MainService.error_404
+handler500 = MainService.error_500
+handler403 = MainService.error_403
+handler400 = MainService.error_400
+
+
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+
+if settings.DEBUG == False:
+    urlpatterns += staticfiles_urlpatterns()
+
+
